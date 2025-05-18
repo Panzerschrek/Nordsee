25aeb31789479a49a14d844f6d80863c8e3c3444
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-05-18 13:18:48 +0300

    Prostavläjem ispolnäjemym fajlam kompilätora flag isplnäjemosti. On teräjetsä gde-to podoroge.

--- ci/build_release.sh
+++ ci/build_release.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Mark used executables with executable bit.
+chmod +x "./compiler_gnu_linux/bin/u.._build_system1" &&\
+chmod +x "./compiler_gnu_linux/bin/u.._compiler1" &&\
+chmod +x "./compiler_gnu_linux/bin/u.._cpp_header_converter" &&\
+# Run the Ü build system.
 ./compiler_gnu_linux/bin/u.._build_system1 build
